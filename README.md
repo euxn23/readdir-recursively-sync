a011c1647b801ed9a5bea10c47df7eaaa9c4dd27
euxn23 <euxn23@gmail.com>
2018-05-21 14:39:59 +0900

    Add flatten function

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
 # readdir-recursively-sync
+
 A simple, zero-dependecy readdir-recursively module.
 
 ## Usage
+
+### Nested Array Path
+
 ```
 const { readdirRecursivelySync } = require('readdir-recursively-sync')
 const files = readdirRecursivelySync('nested/dir/path/')
 ```
 
+### Flatten Array Path
+
+```
+const { readdirRecursivelySyncFlatten } = require('readdir-recursively-sync')
+const files = readdirRecursivelySyncFlatten('nested/dir/path/')
+```
+
 ## Notes
-- arg `option` is same to `fs.readdir`.
-- supports Node.js 8.x+
+
+* arg `option` is same to `fs.readdir`.
+* supports Node.js 8.x+
 
 ## Licence
+
 MIT
--- index.js
+++ index.js
@@ -9,10 +9,24 @@ const readdirRecursivelySync = (baseDir, options = {}) =>
           ? readdirRecursivelySync(`${baseDir}/${path}`, options)
           : `${baseDir}/${path}`
     )
-    .reduce(
-      (prev, next) =>
-        Array.isArray(next) ? [...prev, ...next] : [...prev, next],
-      []
-    )
 
+module.exports = readdirRecursivelySync
 module.exports.readdirRecursivelySync = readdirRecursivelySync
+
+const flatten = paths => {
+  const flattened = paths.reduce(
+    (prev, next) =>
+      Array.isArray(next) ? [...prev, ...next] : [...prev, next],
+    []
+  )
+  return flattened.filter(Array.isArray).length > 0
+    ? flatten(flattened)
+    : flattened
+}
+
+module.exports.flatten = flatten
+
+const readdirRecursivelySyncFlatten = (baseDir, options = {}) =>
+  flatten(readdirRecursivelySync(baseDir, options))
+
+module.exports.readdirRecursivelySyncFlatten = readdirRecursivelySyncFlatten
